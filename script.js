12b4bd5b380a7e51df345a0ba4ddb2b494bcd880
Yampiero Mujica <jeanpiero01@hotmail.com>
2025-12-29 02:42:27 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-	const storageTheme = localStorage.getItem('theme');
-	const systemColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-
-	const newTheme = storageTheme ?? systemColorScheme;
-
-	document.documentElement.setAttribute('data-theme', newTheme);
