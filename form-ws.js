6dd51b2a279414524621c612823ccf0b30d7520c
Yampiero Mujica <jeanpiero01@hotmail.com>
2026-01-02 19:45:16 -0400

    Actualizar form-ws.js

--- form-ws.js
+++ form-ws.js
@@ -11,9 +11,9 @@
             const text = `Hola, me interesa este plan:%0A%0A*Nombre:* ${name}%0A*Teléfono:* ${phone}%0A*Email:* ${email}%0A*Plan:* ${plan}%0A*Mensaje:* ${message}`;
        
 form.reset();
-     
+     window.open(`https://wa.me/584120348988?text=${text}`, '_blank');
       alert('¡Gracias por tu solicitud! Serás redirigido a WhatsApp para confirmar.'); 
-         window.open(`https://wa.me/584120348988?text=${text}`, '_blank');
+         
             
         
         }
