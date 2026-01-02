2a9ed4ffd1aa97ac5fb97563cc646a085938f981
Yampiero Mujica <jeanpiero01@hotmail.com>
2026-01-02 19:36:40 -0400

    Actualizar form-ws.js

--- form-ws.js
+++ form-ws.js
@@ -5,10 +5,10 @@
             const name = form.name.value;
             const phone = form.phone.value;
             const email = form.email.value || 'No proporcionado';
-            const service = form.service.value;
+            const plan = form.plan.value;
             const message = form.message.value || 'No hay mensaje adicional';
             
-            const text = `Hola, estoy interesado/da en un servicio:%0A%0A*Nombre:* ${name}%0A*Teléfono:* ${phone}%0A*Email:* ${email}%0A*Servicio:* ${service}%0A*Mensaje:* ${message}`;
+            const text = `Hola, estoy interesado/da en un servicio:%0A%0A*Nombre:* ${name}%0A*Teléfono:* ${phone}%0A*Email:* ${email}%0A*Plan:* ${plan}%0A*Mensaje:* ${message}`;
             
             window.open(`https://wa.me/584120348988?text=${text}`, '_blank');
             form.reset();
